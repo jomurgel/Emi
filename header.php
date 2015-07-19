5c86a421c6f1632e01ffd10b81b071961290292e
jomurgel <dev@jomurgel.com>
2015-07-19 14:34:17 -0400

    Header updates
    Added schema support
    Slimed down wp_title for functions.php
    Removed favicon in favor of functions.php
    changed location of js
    Added mobile nav support

--- header.php
+++ header.php
@@ -14,41 +14,42 @@
 <html id="ie" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="<?php html_tag_schema(); ?>">
 <!--<![endif]-->
 
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <title><?php wp_title(); ?></title>
     
     <!-- favicon & links -->
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
     <!-- stylesheets are enqueued via functions.php -->
 
     <!-- all other scripts are enqueued via functions.php -->
     <!--[if lt IE 9]>
-        <script src="<?php echo get_template_directory_uri(); ?>/assets/vendor/html5shiv.js" type="text/javascript"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/html5shiv.js" type="text/javascript"></script>
     <![endif]-->
 
     <?php // Lets other plugins and files tie into our theme's <head>:
     wp_head(); ?>
+    
 </head>
  
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage">
 	<div id="page">
-		<header id="site-header" role="banner" class="row">            
+		<header id="site-header" class="row" itemscope="itemscope" itemtype="http://schema.org/WPHeader" role="banner">            
 			<a href="<?php echo esc_url( home_url() ); ?>/" class="logo">
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/assets/images/logo.png'" alt="<?php bloginfo('name'); ?>">
 			</a>
-
-			<nav class="access" role="navigation">
+			
+			<a href="#" id="menu-toggle" role="button"><span>Menu</span></a>
+			<nav class="access" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 			</nav><!-- #access -->  
 		</header><!-- #branding -->
 
 
-		<div id="main" class="row">
+		<main id="main" class="row" role="main" itemprop="mainContentOfPage" itemscope="itemscope" itemtype="http://schema.org/Blog">
