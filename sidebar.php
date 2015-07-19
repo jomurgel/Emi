d6b162e632ad58bb5e93f25f5d9f704038848a5e
jomurgel <dev@jomurgel.com>
2015-07-19 14:38:02 -0400

    Added schema support

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,6 @@
  */
 
 ?>
-<div id="secondary" class="col span_4" role="complementary">
+<div id="secondary" class="col span_4" role="complementary" itemscope="itemscope" itemtype="http://schema.org/WPSideBar">
 	<?php dynamic_sidebar('sidebar-1'); ?>
 </div><!-- #secondary .widget-area -->
