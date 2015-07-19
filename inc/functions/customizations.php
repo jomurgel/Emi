eb7d1efeb79b7eb635dc0b55296276857a4a1d8f
jomurgel <dev@jomurgel.com>
2015-07-19 14:35:10 -0400

    Added Google Font and Typekit support

--- inc/functions/customizations.php
+++ inc/functions/customizations.php
@@ -42,4 +42,30 @@ show_admin_bar( false );
 //    return $query;
 //}
 //add_filter('pre_get_posts','themeFunction_custom_query');
+
+// ADD Google Fonts Support
+//function child_load_google_fonts() {
+//  	
+  	// Setup font arguments
+//	$query_args = array(
+//		'family' => 'Open+Sans:300,400,700' // Change this font to whatever font you'd like
+//	);
+ 
+ 	// A safe way to register a CSS style file for later use
+//	wp_register_style( 'google-fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
+	
+	// A safe way to add/enqueue a CSS style file to a WordPress generated page
+//	wp_enqueue_style( 'google-fonts' );
+//}
+
+// Add Typekit Support
+//function theme_typekit() {
+//    wp_enqueue_script( 'theme_typekit', '//use.typekit.net/xxxxxxx.js');
+//}
+
+//function theme_typekit_inline() {
+/*  if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+  	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+*/}
 ?>
