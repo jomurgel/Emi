94d50d67e163f940573bbaf4406a585e3c58a339
jomurgel <dev@jomurgel.com>
2015-07-19 14:30:14 -0400

    Added schema support

--- content-page.php
+++ content-page.php
@@ -11,7 +11,7 @@
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content" itemprop="text">
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
