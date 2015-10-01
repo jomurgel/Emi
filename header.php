47d6af5e1b61abf2a6f3609e8f81599087d33feb
jomurgel <jomurgel@users.noreply.github.com>
2015-10-01 12:00:55 -0400

    Updated arguments for wp_title

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title><?php wp_title(); ?></title>
+    <title><?php wp_title(' ', true, 'right'); ?></title>
     
     <!-- favicon & links -->
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
